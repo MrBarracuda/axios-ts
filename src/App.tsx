14a90815f8823fb077ffd36f956015a82f87bc83
Dmytro Dobrovolskyi <1dima9999@gmail.com>
2022-09-30 13:24:38 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {FC} from 'react';
+import styled from "styled-components";
 
-function App() {
+
+const Wrapper = styled.section`
+  background: pink;
+  padding: 0 calc(50vw - 590px);
+`
+const Content = styled.div`
+background: aquamarine;
+`
+
+export const App:FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Wrapper><Content>hi</Content></Wrapper>
+  )
 }
 
-export default App;
